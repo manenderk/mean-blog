f7ed49e04b2484ba764b998cb72117b63a9a74d1
manender <manender369@gmail.com>
2019-08-05 23:31:27 -0400

    initiated server

---
+++ my-server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+app.use((req, res, next) => {
+  console.log('First middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send('Hello there');
+})
+
+module.exports = app;
